fdde60ae7fbb8dbb17d3aeef5c670cd731a1a2c3
Matt Morgan <mcubed@outlook.com>
2018-09-09 16:15:04 -0500

    First (incomplete) stab at damage.

---
+++ MurderWithFriendsAPI/Controllers/DamageController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MurderWithFriendsAPI.Services;
+
+namespace MurderWithFriendsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DamageController : ControllerBase
+    {
+
+		private DamageService _damageService;
+
+		public DamageController(DamageService damageService)
+		{
+			_damageService = damageService;
+		}
+
+		// GET: api/Damage
+		[HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Damage/5
+        [HttpGet("{attackerid}/{defenderid}", Name = "Get")]
+        public decimal Get(long attackerId, long defenderId)
+        {
+			return _damageService.CalculateDamage(attackerId, defenderId);
+        }
+
+        // POST: api/Damage
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Damage/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- MurderWithFriendsAPI/Services/DamageService.cs
+++ MurderWithFriendsAPI/Services/DamageService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using MurderWithFriendsAPI.Models;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -8,9 +10,81 @@ namespace MurderWithFriendsAPI.Services
 {
 	public class DamageService
 	{
-		public void CalculateDamage()
-		{
-			Debug.WriteLine("Damn");
+		public decimal CalculateDamage(long attackerID, long defenderID)
+		{			
+			int attackVal = GetAttackValue(attackerID);
+			int defenseVal = 1;
+			return attackVal - defenseVal;
 		}
+
+		private int GetAttackValue(long characterID)
+		{
+			int attackVal = 0;
+			using (var context = new ItsOnlyHeroesContext())
+			{
+				List<Character> achar = context.Character.Where(x => x.CharacterId == characterID)
+					.Include(x => x.BaseStats).ToList();								
+				
+				foreach (var stat in achar.Where(x => x.BaseStats != null))
+				{
+					attackVal += 
+						stat.BaseStats.Agility +
+						stat.BaseStats.Armor +
+						stat.BaseStats.ArmorPenetration +
+						stat.BaseStats.Charisma +
+						stat.BaseStats.Constitution +
+						stat.BaseStats.DarkBonus +
+						stat.BaseStats.Dexterity +
+						stat.BaseStats.EarthBonus +
+						stat.BaseStats.ElectricBonus +
+						stat.BaseStats.FireBonus +
+						stat.BaseStats.HolyBonus +
+						stat.BaseStats.Intelligence +
+						stat.BaseStats.MagicPenetration +
+						stat.BaseStats.MagicResist +
+						stat.BaseStats.Strength +
+						stat.BaseStats.WaterBonus +
+						stat.BaseStats.Wisdom;						
+				}
+			}
+
+			return attackVal;
+		}
+
+
+		private int GetDefenseValue(long characterID)
+		{
+			int defenseVal = 0;
+			using (var context = new ItsOnlyHeroesContext())
+			{
+				List<Character> achar = context.Character.Where(x => x.CharacterId == characterID)
+					.Include(x => x.BaseStats).ToList();
+
+				foreach (var stat in achar.Where(x => x.BaseStats != null))
+				{
+					defenseVal +=
+						stat.BaseStats.Agility +
+						stat.BaseStats.Armor +
+						stat.BaseStats.ArmorPenetration +
+						stat.BaseStats.Charisma +
+						stat.BaseStats.Constitution +
+						stat.BaseStats.DarkBonus +
+						stat.BaseStats.Dexterity +
+						stat.BaseStats.EarthBonus +
+						stat.BaseStats.ElectricBonus +
+						stat.BaseStats.FireBonus +
+						stat.BaseStats.HolyBonus +
+						stat.BaseStats.Intelligence +
+						stat.BaseStats.MagicPenetration +
+						stat.BaseStats.MagicResist +
+						stat.BaseStats.Strength +
+						stat.BaseStats.WaterBonus +
+						stat.BaseStats.Wisdom;
+				}
+			}
+
+			return defenseVal;
+		}
+
 	}
 }
