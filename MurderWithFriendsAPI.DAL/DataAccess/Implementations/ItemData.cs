4e3e55f8167978039f4f66c47da05ca8d9e66fca
Zer0SixX <w.d.hill.3@gmail.com>
2018-09-22 10:14:04 -0500

    Added Items DAL

---
+++ MurderWithFriendsAPI.DAL/DataAccess/Implementations/ItemData.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MurderWithFriendsAPI.DAL.DataAccess.Interfaces;
+using MurderWithFriendsAPI.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MurderWithFriendsAPI.DAL.DataAccess.Implementations
+{
+    class ItemData : IItemData
+    {
+        public Task AddOrUpdateItemAsync(Item item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Item>> GetAllItemsAsync()
+        {
+            using (var context = new ItsOnlyHeroesContext())
+            {
+                var items = await context.Item.Where(x => x.Active == true)
+                    .Include(x => x.ItemType)
+                    .Include(x => x.Stats)
+                    .Include(x => x.BuyCurrency)
+                    .ToListAsync();
+
+                return items;
+            }
+        }
+
+        public async Task<IEnumerable<ItemType>> GetAllItemTypesAsync()
+        {
+            using (var context = new ItsOnlyHeroesContext())
+            {
+                var itemTypes = await context.ItemType.ToListAsync();
+                return itemTypes;
+            }
+        }
+
+        public async Task<Item> GetItemAsync(long itemId)
+        {
+            using (var context = new ItsOnlyHeroesContext())
+            {
+                var items = await context.Item.Where(x => x.Active == true)
+                    .Where(x => x.ItemId == itemId)
+                    .Include(x => x.ItemType)
+                    .Include(x => x.Stats)
+                    .Include(x => x.BuyCurrency)
+                    .FirstOrDefaultAsync();
+
+                return items;
+            }
+        }
+    }
+}
---
+++ MurderWithFriendsAPI.DAL/DataAccess/Interfaces/IItemData.cs
@@ -0,0 +1,19 @@
+﻿using MurderWithFriendsAPI.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MurderWithFriendsAPI.DAL.DataAccess.Interfaces
+{
+    public interface IItemData
+    {
+        Task<IEnumerable<Item>> GetAllItemsAsync();
+
+        Task<IEnumerable<ItemType>> GetAllItemTypesAsync();
+
+        Task AddOrUpdateItemAsync(Item item);
+
+        Task<Item> GetItemAsync(long itemId);
+    }
+}
